3f0145ddf96cea3d141c4764f82f42041aef3c63
Felipe Lamela <fe-fl@outlook.com>
2023-05-07 21:40:36 -0300

    password

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,29 +1,44 @@
 import React from "react";
 
 const Login = ({ usuario }) => {
-
   const [login, setLogin] = React.useState(``);
-  const [password, setPassword] = React.useState(``)
+  const [password, setPassword] = React.useState(``);
 
-
-  const confirmation = (event, usuario) =>{
+  const confirmation = (event, usuario) => {
     event.preventDefault();
-  }
+  };
 
   return (
     <>
-      <form onSubmit={(event) => confirmation(event)} className="formNewContent " action="">
+      <form
+        onSubmit={(event) => confirmation(event)}
+        className="formNewContent "
+        action=""
+      >
         <h2 className="formTitulo ">Login</h2>
         <label className="labelText" htmlFor="login">
           E-mail
         </label>
-        <input className="inputText " type="text" id="login" />
+        <input
+          className="inputText "
+          type="text"
+          id="login"
+          value={login}
+          onChange={(event) => setLogin(event.target.value)}
+        />
 
         <label className="labelText" htmlFor="login">
           Senha
         </label>
-        <input className="inputText " type="password" id="login" />
-        <button className="buttonLogin" type="submit">Login</button>
+        <input
+          type="password"
+          id="login"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button className="buttonLogin" type="submit">
+          Login
+        </button>
       </form>
     </>
   );
